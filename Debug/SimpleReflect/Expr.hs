96c141e0133cd5376ecb718a9d0d404730fd3211
Twan van Laarhoven <twanvl@gmail.com>
2014-04-15 15:46:14 +0200

    Fixed -Wall warnings:  * type signatures for all functions  * fixed name shadow warnings

--- Debug/SimpleReflect/Expr.hs
+++ Debug/SimpleReflect/Expr.hs
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Debug.SimpleReflect.Expr
--- Copyright   :  (c) 2008 Twan van Laarhoven
+-- Copyright   :  (c) 2008-2014 Twan van Laarhoven
 -- License     :  BSD-style
 -- 
 -- Maintainer  :  twanvl@gmail.com
@@ -63,26 +63,33 @@ data Associativity = InfixL | Infix | InfixR deriving Eq
 
 -- | An infix operator with the given associativity, precedence and name
 op :: Associativity -> Int -> String -> Expr -> Expr -> Expr
-op fix prec op a b = emptyExpr { showExpr = showFun }
+op fix prec opName a b = emptyExpr { showExpr = showFun }
  where showFun p = showParen (p > prec)
                      $ showExpr a (if fix == InfixL then prec else prec + 1)
-                     . showString op
+                     . showString opName
                      . showExpr b (if fix == InfixR then prec else prec + 1)
 
 ------------------------------------------------------------------------------
 -- Adding numeric results
 ------------------------------------------------------------------------------
 
+iOp :: (Expr -> Expr) -> (Integer -> Integer) -> Expr -> Expr
+iOp2 :: (Expr -> Expr -> Expr) -> (Integer -> Integer -> Integer) -> Expr -> Expr -> Expr
+dOp :: (Expr -> Expr) -> (Double -> Double) -> Expr -> Expr
+dOp2 :: (Expr -> Expr -> Expr) -> (Double -> Double -> Double) -> Expr -> Expr -> Expr
+
 iOp  r f a   = (r a  ) { intExpr    = f <$> intExpr    a }
 iOp2 r f a b = (r a b) { intExpr    = f <$> intExpr    a <*> intExpr    b }
 dOp  r f a   = (r a  ) { doubleExpr = f <$> doubleExpr a }
 dOp2 r f a b = (r a b) { doubleExpr = f <$> doubleExpr a <*> doubleExpr b }
 
+withReduce :: (Expr -> Expr) -> (Expr -> Expr)
 withReduce r a    = let rr = r a in
                     rr { reduced = withReduce r <$> reduced a
                                <|> fromInteger <$> intExpr    rr
                                <|> fromDouble  <$> doubleExpr rr
                        }
+withReduce2 :: (Expr -> Expr -> Expr) -> (Expr -> Expr -> Expr)
 withReduce2 r a b = let rr = r a b in
                     rr { reduced = (\a' -> withReduce2 r a' b) <$> reduced a
                                <|> (\b' -> withReduce2 r a b') <$> reduced b
@@ -124,7 +131,7 @@ reduce e = maybe e id (reduced e)
 
 -- | Show all reduction steps when evaluating an expression.
 reduction :: Expr -> [Expr]
-reduction e = e : unfoldr (\e -> do e' <- reduced e; return (e',e')) e
+reduction e0 = e0 : unfoldr (\e -> do e' <- reduced e; return (e',e')) e0
 
 ------------------------------------------------------------------------------
 -- Numeric classes
@@ -154,10 +161,10 @@ instance Num Expr where
                      , doubleExpr = Just $ fromInteger i }
 
 instance Real Expr where
-    toRational expr = case (doubleExpr expr, intExpr expr) of
+    toRational someExpr = case (doubleExpr someExpr, intExpr someExpr) of
           (Just d,_) -> toRational d
           (_,Just i) -> toRational i
-          _          -> error "not a number"
+          _          -> error $ "not a rational number: " ++ show someExpr
 
 instance Integral Expr where
     quotRem a b = (quot a b, rem a b)
@@ -166,15 +173,16 @@ instance Integral Expr where
     rem  = withReduce2 $ op InfixL 7 " `rem` "  `iOp2` rem
     div  = withReduce2 $ op InfixL 7 " `div` "  `iOp2` div
     mod  = withReduce2 $ op InfixL 7 " `mod` "  `iOp2` mod
-    toInteger expr = case intExpr expr of
+    toInteger someExpr = case intExpr someExpr of
           Just i -> i
-          _      -> error "not a number"
+          _      -> error $ "not an integer: " ++ show someExpr
 
 instance Fractional Expr where
     (/)   = withReduce2 $ op InfixL 7 " / " `dOp2` (/)
     recip = withReduce  $ fun "recip"  `dOp` recip
     fromRational r = fromDouble (fromRational r)
 
+fromDouble :: Double -> Expr
 fromDouble d = (lift d) { doubleExpr = Just d }
 
 instance Floating Expr where
@@ -215,4 +223,5 @@ instance Bounded Expr where
 instance Monoid Expr where
     mempty = var "mempty"
     mappend = withReduce2 $ op InfixR 6 " <> "
+    mconcat = fun "mconcat"
 
